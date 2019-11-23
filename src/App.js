b844e27f294d3c665098d9b352f24d55173ee6bb
Nikhil Shisode <nikhilshisodemi6@gmail.com>
2019-11-23 22:59:09 +0530

    print env varaible

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div id="main">
+        {console.log(process.env.REACT_APP_API_URL)}
         <h1>Reading List</h1>
         <BookList />
         <AddBook />
